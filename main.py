38e9180717b6e2e91dc90617d306d348fa207edf
Carlos González <cargonm6@teleco.upv.es>
2021-12-14 08:37:35 +0100

    Added MERRA data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import csv
 import math
 from datetime import datetime
 
-pci_columns = {
+pci_cols = {
     "STATE_CODE": None,  # Common parameters
     "SHRP_ID": None,
     "CONSTRUCTION_NO": None,
@@ -12,7 +12,7 @@ pci_columns = {
     "PCI": None
 }
 
-iri_columns = {
+iri_cols = {
     "STATE_CODE": None,  # Common parameters
     "SHRP_ID": None,
     "CONSTRUCTION_NO": None,
@@ -21,7 +21,7 @@ iri_columns = {
     "MRI": None
 }
 
-def_columns = {
+def_cols = {
     "STATE_CODE": None,  # Common parameters
     "SHRP_ID": None,
     "CONSTRUCTION_NO": None,
@@ -37,13 +37,54 @@ def_columns = {
     "PEAK_DEFL_9": None,
 }
 
-skn_columns = {
+skn_cols = {
     "STATE_CODE": None,  # Common parameters
     "SHRP_ID": None,
     "CONSTRUCTION_NO": None,
     "FRICTION_DATE": None,  # Own parameters
     "FRICTION_NO_BEGIN": None,
     "FRICTION_NO_END": None
+}
+
+merra_grid_cols = {
+    "MERRA_ID": None,
+    "STATE_CODE": None,
+    "SHRP_ID": None
+}
+
+merra_temp_cols = {
+    "MERRA_ID": None,
+    "YEAR": None,
+    "TEMP_AVG": None,
+    "FREEZE_INDEX": None,
+    "FREEZE_THAW": None
+}
+
+merra_sola_cols = {
+    "MERRA_ID": None,
+    "YEAR": None,
+    "CLOUD_COVER_AVG": None,
+    "SHORTWAVE_SURFACE_AVG": None
+}
+
+merra_wind_cols = {
+    "MERRA_ID": None,
+    "YEAR": None,
+    "WIND_VELOCITY_AVG": None
+}
+
+merra_prec_cols = {
+    "MERRA_ID": None,
+    "YEAR": None,
+    "PRECIPITATION": None,
+    "EVAPORATION": None,
+    "PRECIP_DAYS": None
+}
+
+merra_humi_cols = {
+    "MERRA_ID": None,
+    "YEAR": None,
+    "REL_HUM_AVG_AVG": None,
 }
 
 
@@ -125,7 +166,7 @@ def add_index(row_ref, rws_index):
     if len(rws_index) > 0:
         # Get the nearest date into rows index array
         nearest, timedelta = near_date([row[1] for row in rws_index],
-                                       to_date(row_ref[pci_columns["SURVEY_DATE"]], "%m/%d/%Y"))
+                                       to_date(row_ref[pci_cols["SURVEY_DATE"]], "%m/%d/%Y"))
         for rw_ind in rws_index:
             # If row index date is the nearest, append to index value array
             if rw_ind[1] == nearest:
@@ -145,9 +186,9 @@ def add_index(row_ref, rws_index):
 
 
 def def_char(row):
-    values = [row[def_columns["PEAK_DEFL_1"]], row[def_columns["PEAK_DEFL_2"]], row[def_columns["PEAK_DEFL_3"]],
-              row[def_columns["PEAK_DEFL_4"]], row[def_columns["PEAK_DEFL_5"]], row[def_columns["PEAK_DEFL_6"]],
-              row[def_columns["PEAK_DEFL_7"]], row[def_columns["PEAK_DEFL_8"]], row[def_columns["PEAK_DEFL_9"]]]
+    values = [row[def_cols["PEAK_DEFL_1"]], row[def_cols["PEAK_DEFL_2"]], row[def_cols["PEAK_DEFL_3"]],
+              row[def_cols["PEAK_DEFL_4"]], row[def_cols["PEAK_DEFL_5"]], row[def_cols["PEAK_DEFL_6"]],
+              row[def_cols["PEAK_DEFL_7"]], row[def_cols["PEAK_DEFL_8"]], row[def_cols["PEAK_DEFL_9"]]]
 
     v_limit = len(values)
 
@@ -174,23 +215,32 @@ if __name__ == '__main__':
     csv_def = load_csv("./csv/def.csv")
     csv_skn = load_csv("./csv/skn.csv")
 
+    csv_merra_humi = load_csv("./csv/merra_humi.csv")
+    csv_merra_sola = load_csv("./csv/merra_sola.csv")
+    csv_merra_prec = load_csv("./csv/merra_prec.csv")
+    csv_merra_temp = load_csv("./csv/merra_temp.csv")
+    csv_merra_wind = load_csv("./csv/merra_wind.csv")
+    csv_merra_grid = load_csv("./csv/merra_grid.csv")
+
     print(" ✓  CSV files loaded in", '%.3f' % (time.time() - start_time), "seconds\n")
 
-    sub_time = time.time()
+    pci_cols = update_dict(pci_cols, csv_pci)
+    iri_cols = update_dict(iri_cols, csv_iri)
+    def_cols = update_dict(def_cols, csv_def)
+    skn_cols = update_dict(skn_cols, csv_skn)
 
-    pci_columns = update_dict(pci_columns, csv_pci)
-    iri_columns = update_dict(iri_columns, csv_iri)
-    def_columns = update_dict(def_columns, csv_def)
-    skn_columns = update_dict(skn_columns, csv_skn)
-
-    print("- ", pci_columns)
-    print("- ", iri_columns)
-    print("- ", def_columns)
-    print("- ", skn_columns)
+    merra_humi_cols = update_dict(merra_humi_cols, csv_merra_humi)
+    merra_sola_cols = update_dict(merra_sola_cols, csv_merra_sola)
+    merra_prec_cols = update_dict(merra_prec_cols, csv_merra_prec)
+    merra_temp_cols = update_dict(merra_temp_cols, csv_merra_temp)
+    merra_wind_cols = update_dict(merra_wind_cols, csv_merra_wind)
+    merra_grid_cols = update_dict(merra_grid_cols, csv_merra_grid)
 
     csv_pci[0].extend(["IRI", "IRI_N", "IRI_D", "IRI_E",
                        "DEF", "DEF_N", "DEF_D", "DEF_E",
-                       "SKN", "SKN_N", "SKN_D", "SKN_E"])
+                       "SKN", "SKN_N", "SKN_D", "SKN_E",
+                       "TEMP_AVG", "FREEZE_INDEX", "FREEZE_THAW", "CLOUD_COVER_AVG", "SHORTWAVE_SURFACE_AVG",
+                       "REL_HUM_AVG_AVG", "PRECIP_DAYS", "PRECIPITATION", "EVAPORATION", "WIND_VELOCITY_AVG"])
 
     limit = 100
 
@@ -203,7 +253,7 @@ if __name__ == '__main__':
         if count == limit:
             break
 
-        if row_pci[pci_columns["STATE_CODE"]] != "1":
+        if row_pci[pci_cols["STATE_CODE"]] != "1":
             continue
 
         # =======================================================================================
@@ -211,14 +261,14 @@ if __name__ == '__main__':
         rws_ind = []
 
         for row_iri in csv_iri[1:]:
-            if [row_pci[pci_columns["STATE_CODE"]],
-                row_pci[pci_columns["SHRP_ID"]],
-                row_pci[pci_columns["CONSTRUCTION_NO"]]] == [row_iri[iri_columns["STATE_CODE"]],
-                                                             row_iri[iri_columns["SHRP_ID"]],
-                                                             row_iri[iri_columns["CONSTRUCTION_NO"]]] \
-                    and row_iri[iri_columns["MRI"]] != "":
-                rws_ind.append([us_is(row_iri[iri_columns["MRI"]].replace(",", "."), 1.609 / 39.37),
-                                to_date(row_iri[iri_columns["VISIT_DATE"]], "%m/%d/%Y")])
+            if [row_pci[pci_cols["STATE_CODE"]],
+                row_pci[pci_cols["SHRP_ID"]],
+                row_pci[pci_cols["CONSTRUCTION_NO"]]] == [row_iri[iri_cols["STATE_CODE"]],
+                                                          row_iri[iri_cols["SHRP_ID"]],
+                                                          row_iri[iri_cols["CONSTRUCTION_NO"]]] \
+                    and row_iri[iri_cols["MRI"]] != "":
+                rws_ind.append([us_is(row_iri[iri_cols["MRI"]].replace(",", "."), 1.609 / 39.37),
+                                to_date(row_iri[iri_cols["VISIT_DATE"]], "%m/%d/%Y")])
 
         row_pci = add_index(row_pci, rws_ind)
 
@@ -227,13 +277,13 @@ if __name__ == '__main__':
         rws_ind = []
 
         for row_def in csv_def[1:]:
-            if [row_pci[pci_columns["STATE_CODE"]],
-                row_pci[pci_columns["SHRP_ID"]],
-                row_pci[pci_columns["CONSTRUCTION_NO"]]] == [row_def[def_columns["STATE_CODE"]],
-                                                             row_def[def_columns["SHRP_ID"]],
-                                                             row_def[def_columns["CONSTRUCTION_NO"]]] \
-                    and row_def[def_columns["PEAK_DEFL_1"]] != "":
-                rws_ind.append([def_char(row_def), to_date(row_def[def_columns["TEST_DATE"]], "%m/%d/%Y")])
+            if [row_pci[pci_cols["STATE_CODE"]],
+                row_pci[pci_cols["SHRP_ID"]],
+                row_pci[pci_cols["CONSTRUCTION_NO"]]] == [row_def[def_cols["STATE_CODE"]],
+                                                          row_def[def_cols["SHRP_ID"]],
+                                                          row_def[def_cols["CONSTRUCTION_NO"]]] \
+                    and row_def[def_cols["PEAK_DEFL_1"]] != "":
+                rws_ind.append([def_char(row_def), to_date(row_def[def_cols["TEST_DATE"]], "%m/%d/%Y")])
 
         row_pci = add_index(row_pci, rws_ind)
 
@@ -242,16 +292,67 @@ if __name__ == '__main__':
         rws_ind = []
 
         for row_skn in csv_skn[1:]:
-            if [row_pci[pci_columns["STATE_CODE"]],
-                row_pci[pci_columns["SHRP_ID"]],
-                row_pci[pci_columns["CONSTRUCTION_NO"]]] == [row_skn[skn_columns["STATE_CODE"]],
-                                                             row_skn[skn_columns["SHRP_ID"]],
-                                                             row_skn[skn_columns["CONSTRUCTION_NO"]]] \
-                    and row_skn[skn_columns["FRICTION_NO_BEGIN"]] != "":
-                rws_ind.append([row_skn[skn_columns["FRICTION_NO_BEGIN"]].replace(",", "."),
-                                to_date(row_skn[skn_columns["FRICTION_DATE"]], "%m/%d/%Y")])
+            if [row_pci[pci_cols["STATE_CODE"]],
+                row_pci[pci_cols["SHRP_ID"]],
+                row_pci[pci_cols["CONSTRUCTION_NO"]]] == [row_skn[skn_cols["STATE_CODE"]],
+                                                          row_skn[skn_cols["SHRP_ID"]],
+                                                          row_skn[skn_cols["CONSTRUCTION_NO"]]] \
+                    and row_skn[skn_cols["FRICTION_NO_BEGIN"]] != "":
+                rws_ind.append([row_skn[skn_cols["FRICTION_NO_BEGIN"]].replace(",", "."),
+                                to_date(row_skn[skn_cols["FRICTION_DATE"]], "%m/%d/%Y")])
 
         row_pci = add_index(row_pci, rws_ind)
+
+        # =======================================================================================
+
+        for row_grid in csv_merra_grid[1:]:
+            if [row_pci[pci_cols["STATE_CODE"]],
+                row_pci[pci_cols["SHRP_ID"]]] == [row_grid[merra_grid_cols["STATE_CODE"]],
+                                                  row_grid[merra_grid_cols["SHRP_ID"]]]:
+                for row_temp in csv_merra_temp[1:]:
+                    if [row_grid[merra_grid_cols["MERRA_ID"]],
+                        row_pci[pci_cols["SURVEY_DATE"]][6:10]] == [row_temp[merra_temp_cols["MERRA_ID"]],
+                                                                    row_temp[merra_temp_cols["YEAR"]]]:
+                        row_pci.extend(
+                            [row_temp[merra_temp_cols["TEMP_AVG"]],
+                             row_temp[merra_temp_cols["FREEZE_INDEX"]],
+                             row_temp[merra_temp_cols["FREEZE_THAW"]]])
+                        break
+
+                for row_sola in csv_merra_sola[1:]:
+                    if [row_grid[merra_grid_cols["MERRA_ID"]],
+                        row_pci[pci_cols["SURVEY_DATE"]][6:10]] == [row_sola[merra_sola_cols["MERRA_ID"]],
+                                                                    row_sola[merra_sola_cols["YEAR"]]]:
+                        row_pci.extend(
+                            [row_sola[merra_sola_cols["CLOUD_COVER_AVG"]],
+                             row_sola[merra_sola_cols["SHORTWAVE_SURFACE_AVG"]]])
+                        break
+
+                for row_humi in csv_merra_humi[1:]:
+                    if [row_grid[merra_grid_cols["MERRA_ID"]],
+                        row_pci[pci_cols["SURVEY_DATE"]][6:10]] == [row_humi[merra_humi_cols["MERRA_ID"]],
+                                                                    row_humi[merra_humi_cols["YEAR"]]]:
+                        row_pci.append(
+                            row_humi[merra_humi_cols["REL_HUM_AVG_AVG"]])
+                        break
+
+                for row_prec in csv_merra_prec[1:]:
+                    if [row_grid[merra_grid_cols["MERRA_ID"]],
+                        row_pci[pci_cols["SURVEY_DATE"]][6:10]] == [row_prec[merra_prec_cols["MERRA_ID"]],
+                                                                    row_prec[merra_prec_cols["YEAR"]]]:
+                        row_pci.extend(
+                            [row_prec[merra_prec_cols["PRECIP_DAYS"]],
+                             row_prec[merra_prec_cols["PRECIPITATION"]],
+                             row_prec[merra_prec_cols["EVAPORATION"]]])
+                        break
+
+                for row_wind in csv_merra_wind[1:]:
+                    if [row_grid[merra_grid_cols["MERRA_ID"]],
+                        row_pci[pci_cols["SURVEY_DATE"]][6:10]] == [row_wind[merra_wind_cols["MERRA_ID"]],
+                                                                    row_wind[merra_wind_cols["YEAR"]]]:
+                        row_pci.append(
+                            row_wind[merra_wind_cols["WIND_VELOCITY_AVG"]])
+                        break
 
         if len(row_pci) != len(csv_pci[0]):
             print("x", end="")
