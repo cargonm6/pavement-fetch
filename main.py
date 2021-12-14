5cf87e2556f7f2341e99071903c7869403fed61c
Carlos González <cargonm6@teleco.upv.es>
2021-12-14 04:05:26 +0100

    Code adapted to calc IRI and DEF values. Improvements implemented

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 
 import csv
+import math
 from datetime import datetime
 
 pci_columns = {
@@ -60,10 +61,14 @@ def save_csv(file, data):
     print("Data saved")
 
 
+def us_is(data, factor):
+    return str(float(data) * factor)
+
+
 def list_equals(items):
     if len(items) > 1:
         for i in range(1, len(items)):
-            if float(items[0].replace(",", ".")) - float(items[i].replace(",", ".")) != 0:
+            if float(items[0]) - float(items[i]) != 0:
                 return False
     return True
 
@@ -103,40 +108,6 @@ def near_date(items, pivot):
         return nearest_over, timedelta_over
 
 
-def put_index(row_ref, csv_ind, p1, p2, p3, p4, p5, date_f):
-    # p1 STATE CODE, p2 SECTION, p3 CONSTRUCTION, p4 VALUE OF INTEREST, p5 DATE
-
-    rws_ind = []
-    for row_ind in csv_ind[1:]:
-        if [row_ref[0], row_ref[2], row_ref[4]] == [row_ind[p1], row_ind[p2], row_ind[p3]] and row_ind[p4] != "":
-            # If it matches all conditions, append a row with VALUE and DATE
-            rws_ind.append([row_ind[p4], to_date(row_ind[p5], date_f)])
-
-    val_ind = []
-    if len(rws_ind) > 0:
-        # Get the nearest date into rows index array
-        nearest, timedelta = near_date([row[1] for row in rws_ind], to_date(row_ref[3], '%m/%d/%Y'))
-        for rw_ind in rws_ind:
-            # If row index date is the nearest, append to index value array
-            if rw_ind[1] == nearest:
-                val_ind.append(rw_ind[0])
-
-        # Data append:
-        # 1. Value
-        # 2. Number of elements that match all conditions
-        # 3. Nearest date flag
-        # 4. Check if all items in the array are the same
-
-        row_ref.extend([val_ind[0], len(rws_ind), timedelta, list_equals(val_ind)])
-
-        # Mean of all values
-        # row_ref.append(sum([float(i.replace(",", ".")) for i in rws_ind]) / len(rws_ind))
-    else:
-        row_ref.extend([None, len(rws_ind), None, None])
-
-    return row_ref
-
-
 def update_dict(p_dict, p_list):
     dict_keys = list(p_dict)
 
@@ -147,6 +118,50 @@ def update_dict(p_dict, p_list):
                 break
 
     return p_dict
+
+
+def add_index(row_ref, rws_index):
+    val_ind = []
+    if len(rws_index) > 0:
+        # Get the nearest date into rows index array
+        nearest, timedelta = near_date([row[1] for row in rws_index],
+                                       to_date(row_ref[pci_columns["SURVEY_DATE"]], "%m/%d/%Y"))
+        for rw_ind in rws_index:
+            # If row index date is the nearest, append to index value array
+            if rw_ind[1] == nearest:
+                val_ind.append(rw_ind[0])
+
+        # Data append:
+        # 1. Value
+        # 2. Number of elements that match all conditions
+        # 3. Time delta
+        # 4. Check if all items in the array are the same
+
+        row_ref.extend([val_ind[0].replace(".", ","), len(rws_index), timedelta, list_equals(val_ind)])
+    else:
+        row_ref.extend([None, len(rws_index), None, None])
+
+    return row_ref
+
+
+def def_char(row):
+    values = [row[def_columns["PEAK_DEFL_1"]], row[def_columns["PEAK_DEFL_2"]], row[def_columns["PEAK_DEFL_3"]],
+              row[def_columns["PEAK_DEFL_4"]], row[def_columns["PEAK_DEFL_5"]], row[def_columns["PEAK_DEFL_6"]],
+              row[def_columns["PEAK_DEFL_7"]], row[def_columns["PEAK_DEFL_8"]], row[def_columns["PEAK_DEFL_9"]]]
+
+    v_limit = len(values)
+
+    for i in range(0, len(values)):
+        if values[i] == "":
+            v_limit = i
+            break
+
+    values = [us_is(float(v.replace(",", ".")), 25.4) for v in values[0:v_limit]]
+
+    row_m = sum([float(v) for v in values])
+    row_s = math.sqrt(sum((float(v) - row_m) ** 2 for v in values) / (len(values) - 1))
+
+    return str(row_m + 2 * row_s)
 
 
 if __name__ == '__main__':
@@ -173,49 +188,87 @@ if __name__ == '__main__':
     print("- ", def_columns)
     print("- ", skn_columns)
 
-    csv_pci[0].extend(["IRI", "DEF", "SKN"])
+    csv_pci[0].extend(["IRI", "IRI_N", "IRI_D", "IRI_E",
+                       "DEF", "DEF_N", "DEF_D", "DEF_E",
+                       "SKN", "SKN_N", "SKN_D", "SKN_E"])
+
+    limit = 100
+
+    table_csv = [csv_pci[0]]
+
+    count = 0
 
     for row_pci in csv_pci[1:]:
+
+        if count == limit:
+            break
+
+        if row_pci[pci_columns["STATE_CODE"]] != "1":
+            continue
+
+        # =======================================================================================
+
+        rws_ind = []
 
         for row_iri in csv_iri[1:]:
             if [row_pci[pci_columns["STATE_CODE"]],
                 row_pci[pci_columns["SHRP_ID"]],
                 row_pci[pci_columns["CONSTRUCTION_NO"]]] == [row_iri[iri_columns["STATE_CODE"]],
                                                              row_iri[iri_columns["SHRP_ID"]],
-                                                             row_iri[iri_columns["CONSTRUCTION_NO"]]]:
-                row_pci.append(row_iri[iri_columns["MRI"]])
+                                                             row_iri[iri_columns["CONSTRUCTION_NO"]]] \
+                    and row_iri[iri_columns["MRI"]] != "":
+                rws_ind.append([us_is(row_iri[iri_columns["MRI"]].replace(",", "."), 1.609 / 39.37),
+                                to_date(row_iri[iri_columns["VISIT_DATE"]], "%m/%d/%Y")])
+
+        row_pci = add_index(row_pci, rws_ind)
+
+        # =======================================================================================
+
+        rws_ind = []
+
+        for row_def in csv_def[1:]:
+            if [row_pci[pci_columns["STATE_CODE"]],
+                row_pci[pci_columns["SHRP_ID"]],
+                row_pci[pci_columns["CONSTRUCTION_NO"]]] == [row_def[def_columns["STATE_CODE"]],
+                                                             row_def[def_columns["SHRP_ID"]],
+                                                             row_def[def_columns["CONSTRUCTION_NO"]]] \
+                    and row_def[def_columns["PEAK_DEFL_1"]] != "":
+                rws_ind.append([def_char(row_def), to_date(row_def[def_columns["TEST_DATE"]], "%m/%d/%Y")])
+
+        row_pci = add_index(row_pci, rws_ind)
+
+        # =======================================================================================
+
+        rws_ind = []
+
+        for row_skn in csv_skn[1:]:
+            if [row_pci[pci_columns["STATE_CODE"]],
+                row_pci[pci_columns["SHRP_ID"]],
+                row_pci[pci_columns["CONSTRUCTION_NO"]]] == [row_skn[skn_columns["STATE_CODE"]],
+                                                             row_skn[skn_columns["SHRP_ID"]],
+                                                             row_skn[skn_columns["CONSTRUCTION_NO"]]] \
+                    and row_skn[skn_columns["FRICTION_NO_BEGIN"]] != "":
+                rws_ind.append([row_skn[skn_columns["FRICTION_NO_BEGIN"]].replace(",", "."),
+                                to_date(row_skn[skn_columns["FRICTION_DATE"]], "%m/%d/%Y")])
+
+        row_pci = add_index(row_pci, rws_ind)
+
+        if len(row_pci) != len(csv_pci[0]):
+            print("x", end="")
+        else:
+            print("|", end="")
+
+        count += 1
+
+        if count % 10 == 0 or count == len(csv_pci[1:limit]):
+            print("", round(count * 100 / len(csv_pci[1:limit]), 2), "%")
+
+        table_csv.append(row_pci)
+
+    save_path = "./res/" + datetime.now().strftime("(%d-%m-%Y_%H-%M-%S)") + "_PCI-IRI-DEF-SK" + ".csv"
+    save_csv(save_path, table_csv)
+
+    print(" ✓  ", str(len(table_csv[1:])), "entries saved to", save_path)
+    print(" ✓  Program finished in", '%.3f' % (time.time() - start_time), "seconds")
 
     exit(0)
-
-    # for table in tables[0][1:]:
-    #     csv_pci[0].extend([table, table + " N", table + " Time Delta", table + " Equal?"])
-    #
-    # limit = 10  # len(csv_pci)
-    #
-    # count = 0
-    #
-    # print("(i) Stepping through the data ...")
-    #
-    # for row_pci in csv_pci[1:limit]:
-    #
-    #     row_pci = put_index(row_pci, csv_iri, 1, 2, 9, 8, 0, '%m/%d/%Y')
-    #     row_pci = put_index(row_pci, csv_def, 0, 1, 8, 21, 2, '%d/%m/%Y')
-    #     row_pci = put_index(row_pci, csv_skn, 1, 0, 3, 10, 4, '%m/%d/%Y')
-    #
-    #     if len(row_pci) != len(csv_pci[0]):
-    #         print("x", end="")
-    #     else:
-    #         print("|", end="")
-    #
-    #     count += 1
-    #
-    #     if count % 10 == 0 or count == len(csv_pci[1:limit]):
-    #         print("", round(count * 100 / len(csv_pci[1:limit]), 2), "%")
-    #
-    # save_path = "./res/" + datetime.now().strftime("(%d-%m-%Y_%H-%M-%S)") + "_PCI-IRI-DEF-SK" + ".csv"
-    # save_csv(save_path, csv_pci[0:limit])
-    #
-    # print("(✓)", str(limit), "entries saved to", save_path)
-    # print("(✓) Program finished in", '%.3f' % (time.time() - start_time), "seconds")
-    #
-    # exit(0)
