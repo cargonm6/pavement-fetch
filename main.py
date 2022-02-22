ca29890ceaf2e357531ea6e73a707fcd0dadffe7
Carlos González <cargonm6@teleco.upv.es>
2022-02-22 12:00:42 +0100

    Joining CSVs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import shutil
+# import shutil
 import sys
 import time
 from datetime import datetime
@@ -74,7 +74,31 @@ def date_diff(d1, d2):
     return (d1 - d2).days
 
 
+def csv_group(p_code, p_path="./csv/", p_path_res="./res/csv/"):
+    print("\n\U0001F6C8 Finding \"" + p_code + "\" files under path \"" + p_path + "\"")
+    p_list = []
+    for root, dirs, files in os.walk(p_path):
+        for f in files:
+            p_list.append((os.path.join(root, f)).replace("\\", "/")) if f.startswith(p_code) else 0
+
+    if len(p_list) == 0:
+        print("(!) Code \"%s\" was not found under path \"%s\"" % (p_code, p_path))
+        return
+
+    p_result = None
+
+    for i in range(0, len(p_list)):
+        if p_result is None:
+            p_result = load_csv(p_list[i])
+        else:
+            [p_result.append(p_row) for p_row in load_csv(p_list[i])[1:]]
+        sys.stdout.write("\r- %d/%d files joined" % (i + 1, len(p_list)))
+
+    save_csv(p_path_res + p_code + "_join.csv", p_result)
+
+
 if __name__ == '__main__':
+
     start_time = time.time()
 
     xls_list = []
@@ -95,8 +119,11 @@ if __name__ == '__main__':
             print("(!) Error with file: ", e)
             continue
 
-    iri_files = [((os.path.join(root, file) if file.startswith("iri") else 0) for file in files) for root, dirs, files
-                 in os.walk("./csv/")]
+    # Join CSV files
+    for code in ["cnd", "def", "iri", "skn", "snu", "trf", "vws"]:
+        csv_group(code)
+
+    exit(0)
 
     #     save_path = "./res/" + os.path.basename(xls_list[k])[0:-5] + ".csv"
     #     shutil.copyfile("./csv/pci.csv", save_path)
